d7b2907270bd1f667638394b5102a4b0e28e31f6
Juho Hänninen <hannisenjuho@gmail.com>
2023-11-21 21:21:27 +0200

    Add possibility to launch multiple processes

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM bytewax/bytewax:0.17.2-python3.11
 
 COPY requirements.txt /tmp/requirements.txt
+COPY entrypoint.sh /bytewax/entrypoint.sh
 
 RUN /venv/bin/pip install -r /tmp/requirements.txt
---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+cd $BYTEWAX_WORKDIR
+. /venv/bin/activate
+python -m bytewax.run $BYTEWAX_PYTHON_FILE_PATH $BYTEWAX_PYTHON_PARAMETERS
+
+echo 'Process ended.'
+
+if [ "$BYTEWAX_KEEP_CONTAINER_ALIVE" = true ]
+then
+    echo 'Keeping container alive...';
+    while :; do sleep 1; done
+fi
