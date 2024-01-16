df08e276a8406fb858b80d64eb4be2e93494ae72
Juho Hänninen <hannisenjuho@gmail.com>
2024-01-16 13:49:47 +0200

    Parse vehicle count and other vehicle's ids

--- src/ekeparser/schemas/stadler_udp.py
+++ src/ekeparser/schemas/stadler_udp.py
@@ -1,18 +1,16 @@
 from .general_parsers import float_parser, int_parser, coordinate_parser, timestamp_str_parser
 from .schema import Schema, FieldParser
 
-CABIN_TYPES = {
-    0b10: "A",
-    0b01: "B",
-    0b11: "AB"
-}
+CABIN_TYPES = {0b10: "A", 0b01: "B", 0b11: "AB"}
+
 
 def doors_parser(content: bytes) -> bool:
+    """Check doors status and returns true if any of the doors is open."""
     # iterate over doors
     for byte in content:
         # check if any of the last 6 bits are set
         if byte & 0x3F:
-            return True # early return if any of the bits are set
+            return True  # early return if any of the bits are set
     return False
 
 def cabin_parser(content: bytes) -> str | None:
@@ -20,11 +18,12 @@ def cabin_parser(content: bytes) -> str | None:
     return CABIN_TYPES.get(bits)
 
 
-def vehicle_parser(content: bytes) -> tuple[int, int]:
-    vehicle_pos_on_train = int_parser(content[0:1])
-    vehicle_no = int_parser(
-        content[vehicle_pos_on_train:vehicle_pos_on_train+1])
-    return vehicle_pos_on_train, vehicle_no
+def vehicle_parser(content: bytes) -> tuple[int, int, int, list[int]]:
+    vehicle_count = int_parser(content[0:1])
+    vehicle_pos_on_train = int_parser(content[1:2])
+    vehicle_numbers = [byte for byte in content[2:6]]
+    vehicle_no = int_parser(content[vehicle_pos_on_train : vehicle_pos_on_train + 1])
+    return vehicle_count, vehicle_pos_on_train, vehicle_no, vehicle_numbers
 
 
 class StadlerUDPSchema(Schema):
@@ -36,7 +35,7 @@ class StadlerUDPSchema(Schema):
         FieldParser(["doors_open"], 21, 28, doors_parser),
         FieldParser(["main_brake_pipe_pressure"], 92, 95, float_parser),
         FieldParser(["active_cabin"], 143, 143, cabin_parser),
-        FieldParser(["vehicle_pos_on_train", "vehicle_no"], 145, 149, vehicle_parser),
+        FieldParser(["vehicle_count", "vehicle_pos_on_train", "vehicle_no", "all_vehicles"], 144, 149, vehicle_parser),
         FieldParser(["train_no"], 156, 157, int_parser),
         FieldParser(["loc_x"], 160, 163, coordinate_parser),
         FieldParser(["loc_y"], 164, 167, coordinate_parser),
