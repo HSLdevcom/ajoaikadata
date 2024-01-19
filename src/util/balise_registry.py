c71426e0e405aedcb4fb66f004109a881bc0e3cd
Juho Hänninen <hannisenjuho@gmail.com>
2024-01-19 13:56:34 +0200

    Update balise registry

--- src/util/balise_registry.py
+++ src/util/balise_registry.py
@@ -3,12 +3,16 @@
 import csv
 from typing import Dict
 
+from .config import read_from_env
+
+(BALISE_DATA_FILE,) = read_from_env(("BALISE_DATA_FILE",), defaults=("./src/util/balise_registry.csv",))
+
 # open csv file balise_registry.csv and read it into a dict using csv.DictReader
 # dict key is balise, value is dict with other data
 balise_registry: Dict[str, dict] = {}
 
 # TODO: Parametrize csv location!
-with open("/bytewax/app/util/balise_registry.csv", "r", newline="") as f:
+with open(BALISE_DATA_FILE, "r", newline="") as f:
     reader = csv.DictReader(f, delimiter=",")
     for row in reader:
         # Store to dict
