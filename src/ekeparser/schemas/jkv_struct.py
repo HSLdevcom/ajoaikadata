6c892f213a308f3e068baead3aa636a8e98c03e2
Juho Hänninen <hannisenjuho@gmail.com>
2024-04-22 13:17:44 +0300

    Parse JKV allowed speed field

--- src/ekeparser/schemas/jkv_struct.py
+++ src/ekeparser/schemas/jkv_struct.py
@@ -8,4 +8,5 @@ class JKVStructSchema(Schema):
     FIELDS = [
         FieldParser(["jkv_target_speed"], 0, 0, partial(int_parser, endian="big")),
         FieldParser(["jkv_speed"], 1, 1, partial(int_parser, endian="big")),
+        FieldParser(["jkv_allowed_speed"], 5, 5, partial(int_parser, endian="big")),
     ]
